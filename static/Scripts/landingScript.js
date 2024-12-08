bcb7e8c2ca79f358a39ead99ca84925d607c7e33
Benjamin Wiegand <126627496+Benjamin-Wiegand@users.noreply.github.com>
2024-12-07 17:39:28 -0800

    show browser name in button, show both buttons for unknown browser

--- static/Scripts/landingScript.js
+++ static/Scripts/landingScript.js
@@ -1,7 +1,5 @@
 // Detect browser & set appropriate download link
 function browserDetect(){
-    let downloadButton = document.getElementById("download-button");
-
     // Sets the user's agent
     let userAgent = navigator.userAgent;
 
@@ -9,18 +7,10 @@ function browserDetect(){
 
     // Verify browser in use & set the link
     if(userAgent.includes("Chrome")){
-        // set download link
-        downloadButton.href = "https://chromewebstore.google.com/detail/web-presentation-clicker/mnkkbdgkehfhenamhggdljdmlpdpjmom";
-
         unhide = document.getElementsByClassName("chrome");
     } else if (userAgent.includes("Firefox")){
-        downloadButton.href = "https://addons.mozilla.org/en-US/firefox/addon/web-presentation-clicker/";
-
         unhide = document.getElementsByClassName("firefox");
     } else {
-        downloadButton.href = "#";
-        downloadButton.textContent = "Browser not supported";
-        
         unhide = document.getElementsByClassName("browser-fallback");
     }
 
